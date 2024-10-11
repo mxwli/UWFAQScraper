b7c412907d02f699a05c39a17a4376759b19ea55
Maxwell Li <maxwli@outlook.com>
2024-10-10 20:29:15 -0400

    fix trainer issue

--- trainer.py
+++ trainer.py
@@ -20,10 +20,10 @@ model_Q = gensim.models.Doc2Vec(documents_Q, vector_size=100, window=8, min_coun
 model_Q.save("models/model_Q_vector_size_100_window_8_min_count_5_workers_20_epochs_17_negative_15_sample_1e-5")
 
 print("generating model A")
-model_A = gensim.models.Doc2Vec(documents_Q, vector_size=100, window=8, min_count=5, workers=20, epochs=17, negative=15, sample=1e-5)
+model_A = gensim.models.Doc2Vec(documents_A, vector_size=100, window=8, min_count=5, workers=20, epochs=17, negative=15, sample=1e-5)
 model_A.save("models/model_A_vector_size_100_window_8_min_count_5_workers_20_epochs_17_negative_15_sample_1e-5")
 
 print("generating model QA")
-model_QA = gensim.models.Doc2Vec(documents_Q, vector_size=100, window=8, min_count=5, workers=20, epochs=17, negative=15, sample=1e-5)
+model_QA = gensim.models.Doc2Vec(documents_QA, vector_size=100, window=8, min_count=5, workers=20, epochs=17, negative=15, sample=1e-5)
 model_QA.save("models/model_QA_vector_size_100_window_8_min_count_5_workers_20_epochs_17_negative_15_sample_1e-5")
 
